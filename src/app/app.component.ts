dec38a51f2f9c12cb72180958cb4ae5beb0a0cb9
yurnak <yuriy.nakonechnyy@gmail.com>
2022-02-12 21:07:28 +0200

    pipes 20 lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,6 @@ import { Component } from '@angular/core';
 
 })
 export class AppComponent {
-  arr = [1, 1, 2, 3, 5, 8, 13]
-  objs = [
-    {title: 'Post 1', author: 'Yuriy', comments:[
-        {name:'Max', text: 'lorem1'},
-        {name:'Max', text: 'lorem2'},
-        {name:'Max', text: 'lorem3'},
-      ]},
-    {title: 'Post 2', author: 'Yuriy 2', comments:[
-        {name:'Max 2', text: 'lorem1'},
-        {name:'Max 2', text: 'lorem2'},
-        {name:'Max 2', text: 'lorem3'},
-      ]}
-  ]
+  now: Date = new Date()
 }
 
