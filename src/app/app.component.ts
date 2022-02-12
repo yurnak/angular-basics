7ff40071a519734511dd0d19805b1e6bed0a59a7
yurnak <yuriy.nakonechnyy@gmail.com>
2022-02-12 14:21:00 +0200

    first upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+
 })
 export class AppComponent {
-  title = 'angular-basics';
+  arr = [1, 1, 2, 3, 5, 8, 13]
+  objs = [
+    {title: 'Post 1', author: 'Yuriy', comments:[
+        {name:'Max', text: 'lorem1'},
+        {name:'Max', text: 'lorem2'},
+        {name:'Max', text: 'lorem3'},
+      ]},
+    {title: 'Post 2', author: 'Yuriy 2', comments:[
+        {name:'Max 2', text: 'lorem1'},
+        {name:'Max 2', text: 'lorem2'},
+        {name:'Max 2', text: 'lorem3'},
+      ]}
+  ]
 }
+
