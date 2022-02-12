0e841325c66f6ac88f05f59fac1228b4ebe2b2ac
yurnak <yuriy.nakonechnyy@gmail.com>
2022-02-12 22:40:54 +0200

    21 lesson Create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostFormComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
